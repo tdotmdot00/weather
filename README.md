1aa73cba4c1896ea078f0881630e672c8fd9af4d
tdotmdot00 <51458672+tdotmdot00@users.noreply.github.com>
2020-02-24 11:20:55 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# @WeatherBot :sunny: :zap: :cloud:
+A slack weather bot that reports the weather of a city of choice using the [OpenWeatherMap API](https://openweathermap.org/).
+
+## Getting Started 
+*This bot is hosted on [Heroku](https://www.heroku.com/), which may take a while for the response :hourglass: . Instructions for running this bot locally could be found below.*
+
+1. If bot is active, type in the bot followed by the city name: **@Weather Bot (CITY-NAME)** 
+  
+    example: **@Weather Bot San Francisco**
+    
+2. The bot will report with the appropriate message. 
+
+# <img src="images/exampleShot.png" height=300>
+
+### To run bot locally
+
+1. Clone this repository. 
+2. Create your own openweatherAPI_Key by creating a free account at [openweatherma](https://openweathermap.org/).
+3. Create your [Slack API Key](https://api.slack.com/apps).
+4. Add in your new keys to your `.bash_profile` in your terminal.
+    ```
+        export SLACK_TOKEN='xoxb************';
+        export OpenWeather_API_Key='644***********';
+
+    ```
+5. Install dependencies: `npm install`
+6. Start running bot: `npm start`
+7. Type in **@Weather Bot (CITY-NAME)** into a Slack channel with the city of choice.
+
+### Built With:
+- [Node.js](https://nodejs.org/en/)
+- [Express](https://www.npmjs.com/package/express)
+- [Axios](https://github.com/axios/axios): Promise based HTTP client for the browser and node.js
+- [Slackbots](https://www.npmjs.com/package/slackbots)
+
+## Design/Architecture & Improvements
+
+- **@Weather Bot** covers basic weather report to the user
+    - High and Low temperature of the day and description of the weather
+- Bot looks for key words in description of weather, such as `cloudy`, to determine emojis and messages to send back to user
+    - Can be improved by making messages more descriptive after more familiarity/research of weather conditions (ex. humidity, pressure, wind, etc) at different places.
+- Deployed using Heroku 
+- Initially, I tried out [Dialogflow](https://dialogflow.com/docs/integrations/slack) to check out if natural language could be used to train the bot. This process covered more than what the basic weather bot intended.
+- Configuration of secret tokens was something that needed to be thought out more.
+    - Can look for ways to improve so users wouldn't have to create new tokens
+    - Useful link from [Heroku](https://devcenter.heroku.com/articles/config-vars#accessing-config-var-values-from-code)
+    - [Storing API keys](https://medium.freecodecamp.org/how-to-securely-store-api-keys-4ff3ea19ebda)
+## License
+This project is licensed under the MIT License.
+
+## Acknowledgements and Resources
+- [Build a Slackbot](https://www.youtube.com/watch?v=nyyXTIL3Hkw): Useful youtube video to help get me started
+- [Learn How to Build Slack Bot Node.js](https://sabe.io/tutorials/learn-how-to-build-slack-bot-node-js): Blog with more details using node.js
+- [Deploying Slack Bots to Heroku](https://blog.heroku.com/how-to-deploy-your-slack-bots-to-heroku#publish-notifications-to-slack)
+- [Resolving crashes](https://help.heroku.com/P1AVPANS/why-is-my-node-js-app-crashing-with-an-r10-error)
+- [How to build a SlackBot](https://medium.com/@alexstroulger/how-to-build-a-slackbot-600635b12a38)
---
+++ index.js
@@ -0,0 +1,121 @@
+const SlackBot = require('slackbots');
+const axios = require('axios');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// initialize bot
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// start server
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+let OpenWeather_API_Key = process.env.OpenWeather_API_Key;
+
+const bot = new SlackBot({
+  token: process.env.SLACK_TOKEN,
+  name: 'weatherbot'
+});
+
+// Start Handler
+// bot.on('start', () => {
+//   bot.postMessage(
+//     'general', 
+//     'Welcome to Weather Bot! Type \'@Weather Bot\' followed by the name of a city to get weather conditions.'
+//   );
+//   botId = bot.self.id;
+// });
+
+// Error handler
+bot.on('error', (err) => console.log('ERROR', err));
+
+// Message Handler
+bot.on('message', (data) => {
+  if (data.type !== 'message') { // will return if data.type is 'error' or 'desktop_notification'
+    return;
+  }
+  if (data.subtype == 'bot_message') { 
+    return;
+  }
+  if (!data.text.includes(botId)) {
+    return;
+  }
+  let message = data.text;
+  let channel = data.channel;
+  
+  if (data.text.includes('help')) {
+    askForHelp(channel);
+    return;
+  }
+  handleMessage(message.replace(/\s*\<.*?\>\s*/g, ''), channel);
+});
+
+// response to data
+function handleMessage(city, channel) {
+  axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OpenWeather_API_Key}`).then(res => {
+    const highTemp = Math.round((res.data.main.temp_max - 273.15) * (9/5) + 32);
+    const lowTemp = Math.round((res.data.main.temp_min - 273.15) * (9/5) + 32);
+    const description = res.data.weather[0].description;
+    const cityName = res.data.name;
+    const country = res.data.sys.country;
+    let emoji = ':thermometer:'; // default
+    let weatherNote = 'Thanks for using Weather Bot!';
+
+    if (description.includes('clear sky')) {
+      emoji = ':sunny: :sunglasses:';
+      weatherNote = ':sunglasses: Be prepared with your sunglasses! :sunglasses:';
+    } else if (description.includes('few clouds')) {
+      emoji = ':sun_behind_cloud:';
+      weatherNote = ':cloud: It might be a bit chilly! :cloud:';
+    } else if (description.includes('scattered') || description.includes('broken')) {
+      emoji = ':cloud:';
+      weatherNote = ':cloud: :cloud: :cloud:';
+    } else if (description.includes('shower')) {
+      emoji = ':shower:';
+      weatherNote = ':umbrella: You might want to bring that umbrella with you! :umbrella:';
+    } else if (description.includes('rain')) {
+      emoji = ':rain_cloud:';
+      weatherNote = ':umbrella: Bring that umbrella! :umbrella:';
+    } else if (description.includes('thunderstorm')) {
+      emoji = ':thunder_cloud_and_rain:';
+      weatherNote = ':coffee: Hot chocolate by the fireplace type of weather :coffee:';
+    } else if (description.includes('snow')) {
+      emoji = ':snowflake:';
+      weatherNote = ':gloves: Time to bundle up! :scarf:'
+    } else if (description.includes('mist')){
+      emoji = ':cloud:';
+      weatherNote = ':cloud: :cloud: :cloud:'
+    }
+
+    if (cityName) {
+      bot.postMessage(
+        channel,
+        `${cityName}, ${country} is at a high ${highTemp}°F and a low of ${lowTemp}°F with ${description} ${emoji}
+        ${weatherNote}`
+      );
+    }
+  }).catch(function () {
+    const params = {
+      icon_emoji: ':speak_no_evil:'
+    };
+    bot.postMessage(
+      channel,
+      `Invalid city. Please check spelling or type in another city.`,
+      params
+    )
+  })
+}
+
+function askForHelp (channel) {
+  const params = {
+    icon_emoji: ':sos:'
+  }
+  bot.postMessage (
+    channel,
+    `Don't fret! Type \'@Weather Bot\' followed by the name of the city.`, 
+    params
+  );
+}
